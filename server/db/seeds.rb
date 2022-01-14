652ae809d0a4a09c6b1114cd3fe3f13dd01c4909
ian038 <ianphua3@gmail.com>
2022-01-14 14:09:09 -0500

    Added data to database

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Book.create(title:"Halo: Silent Storm", author:"Troy Denning", genre:"Action")
+Book.create(title:"Halo: Bad Blood", author:"Matt Forbeck", genre:"Action")
+Book.create(title:"Halo: Mortal Dictata", author:"Karen Gillan", genre:"Action")
+
