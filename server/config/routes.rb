da0a9bb2e63f4bb03bf11e3807446e28e8a694c3
ian038 <ianphua3@gmail.com>
2022-01-14 15:01:04 -0500

    Setup api endpoints

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :items
-  resources :books
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :items
+      resources :books
+    end
+  end
 end
--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,4 +1,3 @@
 Book.create(title:"Halo: Silent Storm", author:"Troy Denning", genre:"Action")
 Book.create(title:"Halo: Bad Blood", author:"Matt Forbeck", genre:"Action")
 Book.create(title:"Halo: Mortal Dictata", author:"Karen Gillan", genre:"Action")
-
